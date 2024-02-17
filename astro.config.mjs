296a0c59dc8dc744f37244c7e81d76890d8456bc
Freddyz5 <freddyltacuri@gmail.com>
2024-02-17 17:50:48 -0500

    build: :rocket: Trying to deploy using github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://freddyz5.github.io',
-  base: '/portafolio',
+  site: 'https://freddyz5.github.io'
 });
