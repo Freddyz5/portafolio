ea6f3a163a7a2bc0edade3f2fae4543b955ef7e6
Freddyz5 <freddyltacuri@gmail.com>
2025-03-08 19:41:34 -0500

    feat: :sparkles: Test three js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://freddyz5.github.io',
   base: 'portafolio',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
