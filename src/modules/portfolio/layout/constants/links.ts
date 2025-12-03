f48b27c8679ad31a252baf8551154785e84effdf
Freddyz5 <freddyltacuri@gmail.com>
2025-12-02 19:28:33 -0500

    feat: Mejorar la gestión de enlaces multilingües en NavBar y Translate

--- src/modules/portfolio/layout/constants/links.ts
+++ src/modules/portfolio/layout/constants/links.ts
@@ -1,6 +1,6 @@
-type Link = {
+export type Link = {
   id: string;
-  name: string;
+  name: { es: string; en: string } | string;
   href: string;
   isIcon?: boolean;
 }
@@ -8,31 +8,31 @@ type Link = {
 export const LINKS: Array<Link> = [
   {
     id: 'home',
-    name: 'Home',
+    name: { es: 'Inicio', en: 'Home' },
     href: '#home',
     isIcon: true
   },
   {
     id: 'experience',
-    name: 'Experiencia',
+    name: { es: 'Experiencia', en: 'Experience' },
     href: '#experience',
     isIcon: false
   },
   {
     id: 'projects',
-    name: 'Proyectos',
+    name: { es: 'Proyectos', en: 'Projects' },
     href: '#projects',
     isIcon: false
   },
-  {
-    id: 'about',
-    name: 'Acerca de mi',
-    href: '#about',
-    isIcon: false
-  },
+  // {
+  //   id: 'about',
+  //   name: 'Acerca de mi',
+  //   href: '#about',
+  //   isIcon: false
+  // },
   {
     id: 'contact',
-    name: 'Contacto',
+    name: { es: 'Contacto', en: 'Contact' },
     href: '#contact',
     isIcon: false
   }
