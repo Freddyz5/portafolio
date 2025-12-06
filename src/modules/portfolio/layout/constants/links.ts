98731e0227eacdc83ef866c3ccb6c21e91b16602
Freddyz5 <freddyltacuri@gmail.com>
2025-12-05 22:43:37 -0500

    feat: Añadir nueva página de proyectos con soporte multilingüe y mejorar componentes de detalle

--- src/modules/portfolio/layout/constants/links.ts
+++ src/modules/portfolio/layout/constants/links.ts
@@ -37,3 +37,24 @@ export const LINKS: Array<Link> = [
     isIcon: false
   }
 ]
+
+export const PROJECT_LINKS: Array<Link> = [
+  {
+    id: 'home',
+    name: { es: 'Inicio', en: 'Home' },
+    href: './',
+    isIcon: true
+  },
+  {
+    id: 'experience',
+    name: { es: 'Experiencia', en: 'Experience' },
+    href: '../portafolio#experience',
+    isIcon: false
+  },
+  {
+    id: 'contact',
+    name: { es: 'Contacto', en: 'Contact' },
+    href: '../portafolio#contact',
+    isIcon: false
+  }
+];
