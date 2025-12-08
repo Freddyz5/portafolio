aff45495d7541386375a08be28ab0f4c0d930099
Freddyz5 <freddyltacuri@gmail.com>
2025-12-08 16:10:11 -0500

    feat: Actualizar configuración de entorno para incluir variables de acceso público

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,22 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 export default defineConfig({
   site: 'https://freddyz5.github.io',
   base: '',
+  env: {
+    schema: {
+      PUBLIC_JSON_ENDPOINT: envField.string({
+        context: "client",
+        access: "public"
+      }),
+      PUBLIC_ACCESS_KEY: envField.string({
+        context: "client",
+        access: "public"
+      }),
+      PUBLIC_ACCESS_KEY_EMAIL: envField.string({
+        context: "client",
+        access: "public"
+      }),
+    }
+  }
 });
