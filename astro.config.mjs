324f48ee5b8f4929ff228536f62cb889e32f00bd
Freddyz5 <freddyltacuri@gmail.com>
2025-12-09 13:14:43 -0500

    Actualizar la base del sitio en la configuración de Astro a la raíz

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig, envField } from 'astro/config';
 
 export default defineConfig({
   site: 'https://freddyz5.github.io',
-  base: 'portafolio',
+  base: '/',
   env: {
     schema: {
       PUBLIC_JSON_ENDPOINT: envField.string({
