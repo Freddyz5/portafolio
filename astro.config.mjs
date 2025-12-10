c5248a8f6b864f611722d93be1fabf42244b577c
Freddyz5 <freddyltacuri@gmail.com>
2025-12-09 22:41:38 -0500

    Corregir la base en la configuración de Astro a '/portafolio'

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig, envField } from 'astro/config';
 
 export default defineConfig({
   site: 'https://freddyz5.github.io',
-  base: '/portfolio',
+  base: '/portafolio',
   env: {
     schema: {
       PUBLIC_JSON_ENDPOINT: envField.string({
